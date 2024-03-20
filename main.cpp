8c131f3992b2ae564a13e41556c8a554b3e5752c
Jared <72281577+jachiaram@users.noreply.github.com>
2024-03-20 13:15:38 -0700

    I broke main, hopefully this fixes it

--- main.cpp
+++ main.cpp
@@ -1,195 +1,104 @@
-#include "util.hpp"
 #include <iostream>
 #include <ostream>
 #include <string>
+#include "util.hpp"
 using std::cin, std::cout, std::cerr, std::endl, std::string;
 
 int main(int argc, char *argv[])
 {
-  int main(int argc, char *argv[])
+
+  // Variables used
+  string command;
+  string subCommand;
+  size_t position;
+
+  int input;
+  double value;
+
+  // Check usage
+  if (argc < 4)
   {
-
-    // Variables used
-    string command;
-    string subCommand;
-    size_t position;
-    // Variables used
-    string command;
-    string subCommand;
-    size_t position;
-
-    int input;
-    double value;
-    int input;
-    double value;
-
-    // Check usage
-    if (argc < 4)
-    {
-      cerr << "Usage: ./program <ifile> <ofile> <mode>" << endl;
-      return 1;
-    }
-    // Check usage
-    if (argc < 4)
-    {
-      cerr << "Usage: ./program <ifile> <ofile> <mode>" << endl;
-      return 1;
-    }
-
-    if (!(std::string(argv[3]) == "0" || std::string(argv[3]) == "1"))
-    {
-      std::cerr << "Error : Incorrect mode" << std::endl;
-      return 1;
-    }
-    if (!(std::string(argv[3]) == "0" || std::string(argv[3]) == "1"))
-    {
-      std::cerr << "Error : Incorrect mode" << std::endl;
-      return 1;
-    }
-
-    // Default greeter
-    greeter();
-    // Default greeter
-    greeter();
-
-    // --------- AUTOMATIC MODE -----------
-    //  USE ARGV0 ARGV1 to open file iostreams
-    if (string(argv[3]) == "0")
-    {
-      // --------- AUTOMATIC MODE -----------
-      //  USE ARGV0 ARGV1 to open file iostreams
-      if (string(argv[3]) == "0")
-      {
-
-        std::cout << "Type \"Help\" to see usage instructions" << std::endl;
-        std::cout << "Type \"Help\" to see usage instructions" << std::endl;
-
-        while (true)
-        {
-          if (!nextCommand(command, subCommand, &position))
-          {
-            cerr << "ERROR: Invalid input" << endl;
-            cerr << "Enter \"HELP\" to print out valid commands" << endl;
-            continue;
-          }
-          while (true)
-          {
-            if (!nextCommand(command, subCommand, &position))
-            {
-              cerr << "ERROR: Invalid input" << endl;
-              cerr << "Enter \"HELP\" to print out valid commands" << endl;
-              continue;
-            }
-
-            if (command == "help")
-            {
-              iprintMenu();
-            }
-            if (command == "help")
-            {
-              iprintMenu();
-            }
-
-            if (command == "stop")
-            {
-              cout << "Exiting...." << endl;
-              return 0;
-            }
-          }
-        }
-        if (command == "stop")
-        {
-          cout << "Exiting...." << endl;
-          return 0;
-        }
-      }
-    }
-
-    // Merged modelProgram.cpp
-    // --------- MANUAL MODE -----------
-    // Merged modelProgram.cpp
-    // --------- MANUAL MODE -----------
-
-    if (string(argv[3]) == "1")
-    {
-      if (string(argv[3]) == "1")
-      {
-
-        cout << "Manual mode selected, enter a value:" << endl;
-        // INIT NUMBER CLASS
-        cout << "Manual mode selected, enter a value:" << endl;
-        // INIT NUMBER CLASS
-
-        // BREAK - ME
-        if (!(cin >> value))
-        {
-          cerr << "ERROR: Inavlid Value" << endl;
-          return 1;
-        }
-        // BREAK - ME
-        if (!(cin >> value))
-        {
-          cerr << "ERROR: Inavlid Value" << endl;
-          return 1;
-        }
-
-        printMenu();
-        printMenu();
-
-        if (!(cin >> input))
-        {
-          cerr << "ERROR: Inavlid Value" << endl;
-          return 1;
-        }
-        if (!(cin >> input))
-        {
-          cerr << "ERROR: Inavlid Value" << endl;
-          return 1;
-        }
-
-        while (input != 0)
-        {
-          switch (input)
-          {
-            while (input != 0)
-            {
-              switch (input)
-              {
-
-              case 1:
-                // convert to binary
-              case 1:
-                // convert to binary
-
-              case 2:
-                // convert to hex
-              case 2:
-                // convert to hex
-
-              default:
-                cout << "Invalid input" << endl;
-                break;
-              default:
-                cout << "Invalid input" << endl;
-                break;
-
-                if (input != 0)
-                {
-                  printMenu();
-                  cin >> input;
-                }
-              }
-            }
-          }
-          if (input != 0)
-          {
-            printMenu();
-            cin >> input;
-          }
-        }
-      }
-    }
-
-    return 0;
-    return 0;
+    cerr << "Usage: ./program <ifile> <ofile> <mode>" << endl;
+    return 1;
   }
+
+  if (!(std::string(argv[3]) == "0" || std::string(argv[3]) == "1"))
+  {
+    std::cerr << "Error : Incorrect mode" << std::endl;
+    return 1;
+  }
+
+  // Default greeter
+  greeter();
+
+  // --------- AUTOMATIC MODE -----------
+  //  USE ARGV0 ARGV1 to open file iostreams
+  if (string(argv[3]) == "0")
+  {
+    std::cout << "Type \"Help\" to see usage instructions" << std::endl;
+
+    while (true)
+    {
+      if (!nextCommand(command, subCommand, &position))
+      {
+        cerr << "ERROR: Invalid input" << endl;
+        cerr << "Enter \"HELP\" to print out valid commands" << endl;
+        continue;
+      }
+      if (command == "help")
+      {
+        iprintMenu();
+      }
+
+      if (command == "stop")
+      {
+        cout << "Exiting...." << endl;
+        return 0;
+      }
+    }
+  }
+
+  // Merged modelProgram.cpp
+  // --------- MANUAL MODE -----------
+
+  if (string(argv[3]) == "1")
+  {
+    cout << "Manual mode selected, enter a value:" << endl;
+    // INIT NUMBER CLASS
+
+    // BREAK - ME
+    if (!(cin >> value))
+    {
+      cerr << "ERROR: Inavlid Value" << endl;
+      return 1;
+    }
+
+    printMenu();
+
+    if (!(cin >> input))
+    {
+      cerr << "ERROR: Inavlid Value" << endl;
+      return 1;
+    }
+
+    while (input != 0)
+    {
+      switch (input)
+      {
+      case 1:
+        // convert to binary
+      case 2:
+        // convert to hex
+      default:
+        cout << "Invalid input" << endl;
+        break;
+      }
+      if (input != 0)
+      {
+        printMenu();
+        cin >> input;
+      }
+    }
+  }
+  return 0;
+}
