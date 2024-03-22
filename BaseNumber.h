cc93cb9a296f9267d745a8f5090f603c016e529e
Jared <72281577+jachiaram@users.noreply.github.com>
2024-03-22 11:41:57 -0700

    added exception handling, and error same type supression

--- BaseNumber.h
+++ BaseNumber.h
@@ -2,72 +2,75 @@
 #ifndef MODELPROGRAM_BASENUMBER_H
 #define MODELPROGRAM_BASENUMBER_H
 
-#include <string>
+#include "util.hpp"
 #include <cmath>
 #include <map>
-#include "util.hpp"
+#include <string>
 
 using namespace std;
 
-class BaseNumber
-{
+class BaseNumber {
 private:
-    string value; // The numeric value in string representation
-    string type;  // The base type ("0b", "0d", "0x")
+  string value; // The numeric value in string representation
+  string type;  // The base type ("0b", "0d", "0x")
 
-    // Conversion methods
-    void toDecimal();
+  // Conversion methods
+  void toDecimal();
 
-    void toBinary();
+  void toBinary();
 
-    void toHex();
+  void toHex();
 
-    inline int toInt()
-    {
-        int result = 0;
-        if (type == "0d")
-        {
-            result = std::stoi(value);
-        }
-        else if (type == "0b")
-        {
-        }
-        else if (type == "0x")
-        {
-        }
-        return result;
-    };
+  inline int toInt() {
+    int result = 0;
+    if (type == "0d") {
+      result = std::stoi(value);
+    } else if (type == "0b") {
+    } else if (type == "0x") {
+    }
+    return result;
+  };
 
 public:
-    // Constructors
-    BaseNumber();                                        // Default constructor if needed
-    BaseNumber(const string &value, const string &type); // Constructor with parameters
+  // Constructors
+  BaseNumber(); // Default constructor if needed
+  BaseNumber(const string &value,
+             const string &type); // Constructor with parameters
 
-    // Changes the type of the number to newType and performs the conversion
-    bool convertTo(const string &newType);
+  // Changes the type of the number to newType and performs the conversion
+  bool convertTo(const string &newType);
 
-    // Accessor methods
-    string getNum() const { return type + value; }              // Combines type and value for display
-    int getValue() const { return std::stoi(value); }           // Converts value to integer (if possible)
-    string getType() const { return type; }                     // Returns the type of the number
-    void setValue(const string &newValue) { value = newValue; } // Sets a new value
+  // Accessor methods
+  string getNum() const {
+    return type + value;
+  } // Combines type and value for display
+  int getValue() const {
+    return std::stoi(value);
+  } // Converts value to integer (if possible)
+  string getType() const { return type; } // Returns the type of the number
+  void setValue(const string &newValue) {
+    value = newValue;
+  } // Sets a new value
 
-    // Overloading + operator
-    inline BaseNumber *operator+(const BaseNumber &other) const
-    {
-        int resultValue = std::stoi(this->value) + std::stoi(other.value);
-        BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
-        num->convertTo(type);
-        return num;
+  // Overloading + operator
+  inline BaseNumber *operator+(const BaseNumber &other) const {
+    int resultValue = std::stoi(this->value) + std::stoi(other.value);
+    BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
+    if (num->type != type) {
+      num->convertTo(type);
     }
+    return num;
+  }
 
-    inline BaseNumber *operator-(const BaseNumber &other) const
-    {
-        int resultValue = std::stoi(this->value) - std::stoi(other.value);
-        BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
-        num->convertTo(type);
-        return num;
+  inline BaseNumber *operator-(const BaseNumber &other) const {
+    int resultValue = std::stoi(this->value) - std::stoi(other.value);
+    BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
+
+    if (num->type != type) {
+      num->convertTo(type);
     }
+    return num;
+  }
 };
 
 #endif // MODELPROGRAM_BASENUMBER_H
