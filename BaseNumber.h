6257d61cca027984b17eb7e985c102deab9871cd
Jared <72281577+jachiaram@users.noreply.github.com>
2024-03-21 22:45:03 -0700

    operator overloads and arith working

--- BaseNumber.h
+++ BaseNumber.h
@@ -1,7 +1,6 @@
+#pragma once
 #ifndef MODELPROGRAM_BASENUMBER_H
 #define MODELPROGRAM_BASENUMBER_H
-
-#pragma once
 
 #include <string>
 #include <cmath>
@@ -10,7 +9,8 @@
 
 using namespace std;
 
-class BaseNumber {
+class BaseNumber
+{
 private:
     string value; // The numeric value in string representation
     string type;  // The base type ("0b", "0d", "0x")
@@ -22,20 +22,52 @@ private:
 
     void toHex();
 
+    inline int toInt()
+    {
+        int result = 0;
+        if (type == "0d")
+        {
+            result = std::stoi(value);
+        }
+        else if (type == "0b")
+        {
+        }
+        else if (type == "0x")
+        {
+        }
+        return result;
+    };
+
 public:
     // Constructors
-    BaseNumber(); // Default constructor if needed
+    BaseNumber();                                        // Default constructor if needed
     BaseNumber(const string &value, const string &type); // Constructor with parameters
 
     // Changes the type of the number to newType and performs the conversion
     bool convertTo(const string &newType);
 
     // Accessor methods
-    string getNum() const { return type + value; } // Combines type and value for display
-    int getValue() const { return std::stoi(value); } // Converts value to integer (if possible)
-    string getType() const { return type; } // Returns the type of the number
+    string getNum() const { return type + value; }              // Combines type and value for display
+    int getValue() const { return std::stoi(value); }           // Converts value to integer (if possible)
+    string getType() const { return type; }                     // Returns the type of the number
     void setValue(const string &newValue) { value = newValue; } // Sets a new value
+
+    // Overloading + operator
+    inline BaseNumber *operator+(const BaseNumber &other) const
+    {
+        int resultValue = std::stoi(this->value) + std::stoi(other.value);
+        BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
+        num->convertTo(type);
+        return num;
+    }
+
+    inline BaseNumber *operator-(const BaseNumber &other) const
+    {
+        int resultValue = std::stoi(this->value) - std::stoi(other.value);
+        BaseNumber *num = new BaseNumber(std::to_string(resultValue), "0d");
+        num->convertTo(type);
+        return num;
+    }
 };
 
-
-#endif //MODELPROGRAM_BASENUMBER_H
+#endif // MODELPROGRAM_BASENUMBER_H
--- main.cpp
+++ main.cpp
@@ -6,55 +6,78 @@
 
 using namespace std;
 
-BaseNumber *getInput() {
+BaseNumber *getInput()
+{
     string inVal;
 
     // Loop until valid input is received
-    while (true) {
+    while (true)
+    {
         cout << "Enter value and type (Prefix with 0x for hex, 0b for binary, or 0d for decimal)\n> ";
         cin >> inVal;
 
         if (inVal.length() > 2 &&
-            (inVal.substr(0, 2) == "0x" || inVal.substr(0, 2) == "0b" || inVal.substr(0, 2) == "0d")) {
+            (inVal.substr(0, 2) == "0x" || inVal.substr(0, 2) == "0b" || inVal.substr(0, 2) == "0d"))
+        {
             string type = inVal.substr(0, 2);
             string value = inVal.substr(2);
 
             return new BaseNumber(value, type);
-        } else {
+        }
+        else
+        {
             cout << "Invalid input. Try again." << endl;
         }
     }
 }
 
-
-void arithFunct(BaseNumber *num1) {
+BaseNumber arithFunct(BaseNumber *num1)
+{
+    string typeTo = num1->getType();
     cout << "Enter a number: " << endl;
     BaseNumber *num2 = getInput();
-    BaseCalc temp(num1, num2);
+    BaseNumber *result = new BaseNumber("0", "0d");
+    // BaseCalc temp(num1, num2);
+
+    num1->convertTo("0d");
+    num2->convertTo("0d");
 
     int select;
     cout << "Choose a function to perform:" << endl;
     cout << "[0] add" << endl;
     cout << "[1] sub" << endl;
-    cin >> select;
 
-    if (select == 0) {
-        temp.addNum();
-    } else if (select == 1) {
-        temp.subNum();
-    } else {
-        cout << "Incorrect input" << endl;
-        return;
+    while (1)
+    {
+        cin >> select;
+
+        if (select == 0)
+        {
+            result = *num1 + *num2;
+            break;
+        }
+        else if (select == 1)
+        {
+            result = *num1 - *num2;
+            break;
+        }
+        else
+        {
+            cout << "Incorrect input, try again" << endl;
+        }
     }
 
-    cout << temp.getResult() << endl;
-
+    result->convertTo(typeTo);
+    cout << result->getNum() << endl;
+    return *result;
 }
 
-int getMenuChoice() {
+int getMenuChoice()
+{
     int choice;
     cout << "Enter your choice: ";
-    while (!(cin >> choice)) {
+    while (!(cin >> choice))
+    {
         cin.clear(); // Clear the error flag
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid input. Please enter a number: ";
@@ -62,80 +85,94 @@ int getMenuChoice() {
     return choice;
 }
 
-void handleMenuChoice(int choice, BaseNumber *currentNum) {
+void handleMenuChoice(int choice, BaseNumber *currentNum)
+{
     // Handle the choice here. For example:
-    switch (choice) {
-        case 1:
-            cout << "Converting to binary..." << endl;
-            currentNum->convertTo("0b");
-            cout << currentNum->getNum() << endl;
-            break;
-        case 2:
-            cout << "Converting to hex..." << endl;
-            currentNum->convertTo("0x");
-            cout << currentNum->getNum() << endl;
-            break;
-        case 3:
-            cout << "Converting to decimal..." << endl;
-            currentNum->convertTo("0d");
-            cout << currentNum->getNum() << endl;
-            break;
-        case 4:
-            cout << "Performing arithmetic operations..." << endl;
-            arithFunct(currentNum);
-            break;
-        default:
-            cout << "Invalid choice." << endl;
-            break;
+    switch (choice)
+    {
+    case 1:
+        cout << "Converting to binary..." << endl;
+        currentNum->convertTo("0b");
+        cout << currentNum->getNum() << endl;
+        break;
+    case 2:
+        cout << "Converting to hex..." << endl;
+        currentNum->convertTo("0x");
+        cout << currentNum->getNum() << endl;
+        break;
+    case 3:
+        cout << "Converting to decimal..." << endl;
+        currentNum->convertTo("0d");
+        cout << currentNum->getNum() << endl;
+        break;
+    case 4:
+        cout << "Performing arithmetic operations..." << endl;
+        *currentNum = arithFunct(currentNum);
+        break;
+    default:
+        cout << "Invalid choice." << endl;
+        break;
     }
 }
 
-int main(int argc, char **argv) {
-    if (argc < 4) {
+int main(int argc, char **argv)
+{
+    if (argc < 4)
+    {
         cerr << "Usage: ./program <ifile> <ofile> <mode>" << endl;
         return 1;
     }
 
     string mode(argv[3]);
-    if (mode != "0" && mode != "1") {
+    if (mode != "0" && mode != "1")
+    {
         cerr << "Error: Incorrect mode" << endl;
         return 1;
     }
 
     greeter();
 
-    if (mode == "1") {
+    if (mode == "1")
+    {
         cout << "Manual mode selected!" << endl;
         BaseNumber *currentNum = getInput();
 
         int choice = -1;
-        while (choice != 0) {
+        while (choice != 0)
+        {
             printMenu();
             choice = getMenuChoice();
-            if (choice == 0) {
+            if (choice == 0)
+            {
                 cout << "Exiting..." << endl;
                 break;
             }
             handleMenuChoice(choice, currentNum);
         }
         delete currentNum;
-    } else if (mode == "0") {
+    }
+    else if (mode == "0")
+    {
         cout << "Automatic mode selected. Type \"Help\" to see usage instructions." << endl;
 
         string command, subCommand;
         size_t position;
-        while (true) {
-            if (!nextCommand(command, subCommand, &position)) {
+        while (true)
+        {
+            if (!nextCommand(command, subCommand, &position))
+            {
                 cerr << "ERROR: Invalid input. Enter \"HELP\" to print out valid commands." << endl;
                 continue;
             }
 
-            if (command == "hex") {
+            if (command == "hex")
+            {
                 // this should convert all numbers in ifile to hex regardless of
                 // starting base
             }
-            //TODO:....rest
-            if (command == "stop") {
+            // TODO:....rest
+            if (command == "stop")
+            {
                 cout << "Exiting..." << endl;
                 break;
             }
